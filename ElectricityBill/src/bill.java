ba7ecdef96ce8abf1306107c314b5039d8e2d5a5
jidaga naveen <jidaganaveen4448@gmail.com>
2023-12-17 18:43:06 +0530

    created bill project

---
+++ ElectricityBill/src/bill.java
@@ -0,0 +1,17 @@
+
+public class bill {
+
+	public static void main(String[] args) {
+		int units = 230;
+		double billToPay = 0;
+		if(units < 100)
+		billToPay = units * 1.20;
+		else if(units < 300)
+		billToPay = 100 * 1.20 + (units - 100) * 2;
+		System.out.println("The electricity bill for "+units+ " is : " + billToPay);
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+
+	}
+
+}
